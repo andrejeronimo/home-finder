733be72ca442efc243700a77648e6fcaebff6e17
Jerónimo <andre.jeronimo.93@gmail.com>
2019-08-05 14:52:36 -0700

    Integration with django-telegrambot library.

--- homefinder/settings.py
+++ homefinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q4oq%4xoj1hxsmw)i&!pm)&06o8uo37ds=lx2xs2&w94&k4#^s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['', '167.71.75.224']
 
 
 # Application definition
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'bot',
+
+    # Externals libraries
+    'django_telegrambot',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,8 @@ ROOT_URLCONF = 'homefinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +125,66 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Telegram Bot settings
+# https://github.com/JungDev/django-telegrambot
+DJANGO_TELEGRAMBOT = {
+
+    'MODE': 'POLLING',
+    'WEBHOOK_SITE': '167.71.75.224',
+    'WEBHOOK_PREFIX': '/bot',
+    #'WEBHOOK_CERTIFICATE': 'cert.pem', # If your site use self-signed
+                         #certificate, must be set with location of your public key
+                         #certificate.(More info at https://core.telegram.org/bots/self-signed )
+    'STRICT_INIT': True,
+    'BOTS': [
+        {
+           'TOKEN': '907970504:AAGc14lZ_3Onqw07aD3FXbQ0DfL3QeF3zcQ',
+
+           #'ALLOWED_UPDATES':(Optional[list[str]]), # List the types of
+                                                   #updates you want your bot to receive. For example, specify
+                                                   #``["message", "edited_channel_post", "callback_query"]`` to
+                                                   #only receive updates of these types. See ``telegram.Update``
+                                                   #for a complete list of available update types.
+                                                   #Specify an empty list to receive all updates regardless of type
+                                                   #(default). If not specified, the previous setting will be used.
+                                                   #Please note that this parameter doesn't affect updates created
+                                                   #before the call to the setWebhook, so unwanted updates may be
+                                                   #received for a short period of time.
+
+           #'TIMEOUT':(Optional[int|float]), # If this value is specified,
+                                   #use it as the read timeout from the server
+
+           #'WEBHOOK_MAX_CONNECTIONS':(Optional[int]), # Maximum allowed number of
+                                   #simultaneous HTTPS connections to the webhook for update
+                                   #delivery, 1-100. Defaults to 40. Use lower values to limit the
+                                   #load on your bot's server, and higher values to increase your
+                                   #bot's throughput.
+
+           # 'MESSAGEQUEUE_ENABLED':(Optinal[bool]), # Make this True if you want to use messagequeue
+
+           # 'MESSAGEQUEUE_ALL_BURST_LIMIT':(Optional[int]), # If not provided 29 is the default value
+
+           # 'MESSAGEQUEUE_ALL_TIME_LIMIT_MS':(Optional[int]), # If not provided 1024 is the default value
+
+           # 'MESSAGEQUEUE_REQUEST_CON_POOL_SIZE':(Optional[int]), # If not provided 8 is the default value
+
+           # 'POLL_INTERVAL' : 1.0, # Time to wait between polling updates from Telegram in
+                           #seconds. Default is 0.0
+
+           #'POLL_CLEAN':(Optional[bool]), # Whether to clean any pending updates on Telegram servers before
+                                   #actually starting to poll. Default is False.
+
+           #'POLL_BOOTSTRAP_RETRIES':(Optional[int]), # Whether the bootstrapping phase of the `Updater`
+                                   #will retry on failures on the Telegram server.
+                                   #|   < 0 - retry indefinitely
+                                   #|     0 - no retries (default)
+                                   #|   > 0 - retry up to X times
+
+           #'POLL_READ_LATENCY':(Optional[float|int]), # Grace time in seconds for receiving the reply from
+                                   #server. Will be added to the `timeout` value and used as the read timeout from
+                           #server (Default: 2).
+        },
+    ],
+}
