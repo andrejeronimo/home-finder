f45644e7ecc0c0caf22b212a7cb0ecdb06bd5773
Jerónimo <andre.jeronimo.93@gmail.com>
2019-08-06 13:33:36 -0700

    Added crawlers app.

---
+++ crawlers/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from crawlers.models import Crawler
+from crawlers.models import Task
+
+
+class CrawlerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'url']
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'crawler']
+
+
+admin.site.register(Crawler, CrawlerAdmin)
+admin.site.register(Task, TaskAdmin)
---
+++ crawlers/models.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+
+from django.db import models
+
+from users.models import User
+
+
+class Crawler(models.Model):
+
+    # Name
+    name = models.CharField(max_length=50, blank=False, null=False, unique=True)
+
+    # Url
+    url = models.CharField(max_length=200, blank=False, null=False)
+
+    # XPATH fields
+
+    # Articles
+    articles = models.CharField(max_length=200, blank=False, null=False)
+
+    # Article id
+    article_id = models.CharField(max_length=200, blank=False, null=False)
+
+    # Article url
+    article_url = models.CharField(max_length=200, blank=False, null=False)
+
+    # Article title
+    article_title = models.CharField(max_length=200, blank=True, null=True)
+
+    # Article image
+    article_image = models.CharField(max_length=200, blank=True, null=True)
+
+    # Article description
+    article_description = models.CharField(max_length=200, blank=True, null=True)
+
+    # Article price
+    article_price = models.CharField(max_length=200, blank=True, null=True)
+
+    # Next page url
+    next_page_url = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+
+    # User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Crawler
+    crawler = models.ForeignKey(Crawler, on_delete=models.CASCADE)
+
+    # Search url
+    search_url = models.CharField(max_length=200, blank=False, null=False)
+
+    # Time interval in minutes (default is 30 minutes)
+    time_interval = models.PositiveIntegerField(blank=False, null=False, default=30)
+
+    # Timestamp
+    timestamp = models.DateTimeField(null=True, blank=True, default=None)
+
+    # Active flag
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.pk)
+
+    def update_timestamp(self):
+        self.timestamp = datetime.now()
+        self.save(update_fields=['timestamp'])
+
+
+class Article(models.Model):
+
+    # Task
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    # Article id
+    article_id = models.CharField(max_length=200, blank=False, null=False)
+
+    class Meta:
+        unique_together = ('task', 'article_id')
+
+
+class ArticleSchema(object):
+
+    def __init__(self, id=None, url=None, title=None, image=None, description=None, price=None):
+        self.id = id
+        self.url = url
+        self.title = title
+        self.image = image
+        self.description = description
+        self.price = price
